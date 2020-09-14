c035498f99bd6af95d2f0c6765849e7fd58f94dc
Eduardo <educdiogo@hotmail.com>
2020-09-14 10:17:38 -0300

    propriedades

--- nivel01/frontend/src/App.js
+++ nivel01/frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from "./components/Header";
 export default function App() {
 	return (
 		<>
-			<Header />
-			<Header />
+			<Header title="Homepage">
+				<ul>
+					<li>Homepage</li>
+					<li>Projects</li>
+					<li>login</li>
+				</ul>
+			</Header>
+			<Header title="Projects">
+				<ul>
+					<li>Homepage</li>
+					<li>Projects</li>
+				</ul>
+			</Header>
 		</>
 	);
 }
