6b3d5f333e882464350e1df7c00fa4232ba535c4
Eduardo <educdiogo@hotmail.com>
2020-09-14 10:10:56 -0300

    componentização

--- nivel01/frontend/src/index.js
+++ nivel01/frontend/src/index.js
@@ -1,4 +1,5 @@
-const soma = (a, b) => {
-	return a + b;
-};
-console.log(soma(1, 5));
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+
+render(<App />, document.getElementById("app"));
