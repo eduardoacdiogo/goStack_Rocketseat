14e7e52dff098606700653bf15891bc61a88a04e
educdiogo@hotmail.com <educdiogo@hotmail.com>
2020-11-27 02:01:44 -0300

    estilizando dashboard

--- nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,63 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/37848906?s=460&u=ab88902851e7f9e3d3742a60cd4dcd6a73d5437b&v=4"
+            alt="Eduardo Andre"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/37848906?s=460&u=ab88902851e7f9e3d3742a60cd4dcd6a73d5437b&v=4"
+            alt="Eduardo Andre"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/37848906?s=460&u=ab88902851e7f9e3d3742a60cd4dcd6a73d5437b&v=4"
+            alt="Eduardo Andre"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
