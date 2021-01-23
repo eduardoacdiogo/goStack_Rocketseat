8992e4e0501b0f7f2b96814f31c71b3f8d7ad5b1
Eduardo Andre <educdiogo@hotmail.com>
2021-01-23 17:36:34 -0300

    configurando rotas

--- nivel03/iniciando-projeto-frontend-web/06-gobarber-web/src/pages/signin/index.tsx
+++ nivel03/iniciando-projeto-frontend-web/06-gobarber-web/src/pages/signin/index.tsx
@@ -45,13 +45,13 @@ const SignIn: React.FC = () => {
           email: data.email,
           password: data.password,
         });
-
-        // removeToast();
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
 
           formRef.current?.setErrors(errors);
+
+          return;
         }
         addToast({
           type: 'error',
@@ -61,7 +61,7 @@ const SignIn: React.FC = () => {
         });
       }
     },
-    [signIn, addToast, removeToast],
+    [signIn, addToast],
   );
 
   return (
